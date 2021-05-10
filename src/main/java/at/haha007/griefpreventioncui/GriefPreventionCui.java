0fcb537bab551fa72f5d679038995cd78b91be07
Haha007 <nichtechtanonym@gmail.com>
2021-05-10 14:30:26 +0200

    check for world

--- src/main/java/at/haha007/griefpreventioncui/GriefPreventionCui.java
+++ src/main/java/at/haha007/griefpreventioncui/GriefPreventionCui.java
@@ -13,91 +13,92 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class GriefPreventionCui extends JavaPlugin implements Listener {
-	int distance;
+    int distance;
 
 
-	@Override
-	public void onEnable() {
-		saveDefaultConfig();
-		reloadConfig();
-		distance = getConfig().getInt("distance", 3);
-		getServer().getPluginManager().registerEvents(this, this);
-		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, this::displayClaims, 10, 10);
-	}
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        reloadConfig();
+        distance = getConfig().getInt("distance", 3);
+        getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, this::displayClaims, 10, 10);
+    }
 
-	private Collection<Claim> getNearbyClaims(Location center) {
-		Collection<Claim> claims = new ArrayList<>();
-		int radius = distance;
-		Chunk lowest = center.subtract(radius, 0, radius).getChunk();
-		Chunk highest = center.add(radius * 2, 0, radius * 2).getChunk();
-		DataStore dataStore = GriefPrevention.instance.dataStore;
-		for (int chunk_x = lowest.getX(); chunk_x <= highest.getX(); chunk_x++) {
-			for (int chunk_z = lowest.getZ(); chunk_z <= highest.getZ(); chunk_z++) {
-				claims.addAll(
-					dataStore.
-						getClaims(chunk_x, chunk_z).
-						stream().
-						filter(c -> c.inDataStore).
-						collect(Collectors.toList()));
-			}
-		}
-		return claims;
-	}
-
-
-	private void displayClaims() {
-		Bukkit.getOnlinePlayers().stream().
-			filter(player -> player.hasPermission("gpcui.display")).
-			filter(player -> player.getInventory().getItemInMainHand().getType() == Material.GOLDEN_SHOVEL).
-			forEach(player -> getNearbyClaims(player.getLocation()).
-				stream().distinct().spliterator().
-				forEachRemaining(claim -> displayClaim(player, claim, false)));
-	}
+    private Collection<Claim> getNearbyClaims(Location center) {
+        Collection<Claim> claims = new ArrayList<>();
+        int radius = distance;
+        Chunk lowest = center.subtract(radius, 0, radius).getChunk();
+        Chunk highest = center.add(radius * 2, 0, radius * 2).getChunk();
+        DataStore dataStore = GriefPrevention.instance.dataStore;
+        World world = center.getWorld();
+        for (int chunk_x = lowest.getX(); chunk_x <= highest.getX(); chunk_x++) {
+            for (int chunk_z = lowest.getZ(); chunk_z <= highest.getZ(); chunk_z++) {
+                claims.addAll(dataStore.
+                        getClaims(chunk_x, chunk_z).
+                        stream().
+                        filter(c -> c.getLesserBoundaryCorner().getWorld() == world).
+                        filter(c -> c.inDataStore).
+                        collect(Collectors.toList()));
+            }
+        }
+        return claims;
+    }
 
 
-	private void displayClaim(Player player, Claim claim, boolean subclaim) {
-		Location max = claim.getLesserBoundaryCorner();
-		Location min = claim.getGreaterBoundaryCorner();
-		int minX = Integer.min(max.getBlockX(), min.getBlockX());
-		int minZ = Integer.min(max.getBlockZ(), min.getBlockZ());
-		int maxX = Integer.max(max.getBlockX(), min.getBlockX()) + 1;
-		int maxZ = Integer.max(max.getBlockZ(), min.getBlockZ()) + 1;
+    private void displayClaims() {
+        Bukkit.getOnlinePlayers().stream().
+                filter(player -> player.hasPermission("gpcui.display")).
+                filter(player -> player.getInventory().getItemInMainHand().getType() == Material.GOLDEN_SHOVEL).
+                forEach(player -> getNearbyClaims(player.getLocation()).
+                        stream().distinct().spliterator().
+                        forEachRemaining(claim -> displayClaim(player, claim, false)));
+    }
 
-		Particle particle = subclaim ? Particle.SOUL_FIRE_FLAME : Particle.FLAME;
-		displayZSurface(maxZ, player, minX, maxX, particle);
-		displayZSurface(minZ, player, minX, maxX, particle);
-		displayXSurface(maxX, player, minZ, maxZ, particle);
-		displayXSurface(minX, player, minZ, maxZ, particle);
-		//recursively execute for subclaims
-		claim.children.stream().filter(c -> c.isNear(player.getLocation(), distance)).forEach(c -> displayClaim(player, c, true));
-	}
 
-	private void displayZSurface(int z, Player player, int minX, int maxX, Particle particle) {
-		if (Math.abs(player.getLocation().getBlockZ() - z) > distance) return;
-		Location center = player.getLocation();
-		int x = center.getBlockX();
-		int y = center.getBlockY();
-		center.setZ(z);
-		int max = Math.min(maxX, x + distance);
-		for (int px = Math.max(x - distance, minX); px <= max; px++) {
-			if (px < minX || px > maxX) continue;
-			for (int py = y - 2; py <= y + 1; py++) {
-				player.spawnParticle(particle, new Location(center.getWorld(), px, py, z), 3, .2, 0, 0, 0);
-			}
-		}
-	}
+    private void displayClaim(Player player, Claim claim, boolean subclaim) {
+        Location max = claim.getLesserBoundaryCorner();
+        Location min = claim.getGreaterBoundaryCorner();
+        int minX = Integer.min(max.getBlockX(), min.getBlockX());
+        int minZ = Integer.min(max.getBlockZ(), min.getBlockZ());
+        int maxX = Integer.max(max.getBlockX(), min.getBlockX()) + 1;
+        int maxZ = Integer.max(max.getBlockZ(), min.getBlockZ()) + 1;
 
-	private void displayXSurface(int x, Player player, int minZ, int maxZ, Particle particle) {
-		if (Math.abs(player.getLocation().getBlockX() - x) > distance) return;
-		Location center = player.getLocation();
-		int z = center.getBlockZ();
-		int y = center.getBlockY();
-		center.setX(x);
-		int max = Math.min(maxZ, z + distance);
-		for (int pz = Math.max(z - distance, minZ); pz <= max; pz++) {
-			for (int py = y - 2; py <= y + 1; py++) {
-				player.spawnParticle(particle, new Location(center.getWorld(), x, py, pz), 3, 0, 0, .2, 0);
-			}
-		}
-	}
+        Particle particle = subclaim ? Particle.SOUL_FIRE_FLAME : Particle.FLAME;
+        displayZSurface(maxZ, player, minX, maxX, particle);
+        displayZSurface(minZ, player, minX, maxX, particle);
+        displayXSurface(maxX, player, minZ, maxZ, particle);
+        displayXSurface(minX, player, minZ, maxZ, particle);
+        //recursively execute for subclaims
+        claim.children.stream().filter(c -> c.isNear(player.getLocation(), distance)).forEach(c -> displayClaim(player, c, true));
+    }
+
+    private void displayZSurface(int z, Player player, int minX, int maxX, Particle particle) {
+        if (Math.abs(player.getLocation().getBlockZ() - z) > distance) return;
+        Location center = player.getLocation();
+        int x = center.getBlockX();
+        int y = center.getBlockY();
+        center.setZ(z);
+        int max = Math.min(maxX, x + distance);
+        for (int px = Math.max(x - distance, minX); px <= max; px++) {
+            if (px < minX || px > maxX) continue;
+            for (int py = y - 2; py <= y + 1; py++) {
+                player.spawnParticle(particle, new Location(center.getWorld(), px, py, z), 3, .2, 0, 0, 0);
+            }
+        }
+    }
+
+    private void displayXSurface(int x, Player player, int minZ, int maxZ, Particle particle) {
+        if (Math.abs(player.getLocation().getBlockX() - x) > distance) return;
+        Location center = player.getLocation();
+        int z = center.getBlockZ();
+        int y = center.getBlockY();
+        center.setX(x);
+        int max = Math.min(maxZ, z + distance);
+        for (int pz = Math.max(z - distance, minZ); pz <= max; pz++) {
+            for (int py = y - 2; py <= y + 1; py++) {
+                player.spawnParticle(particle, new Location(center.getWorld(), x, py, pz), 3, 0, 0, .2, 0);
+            }
+        }
+    }
 }
